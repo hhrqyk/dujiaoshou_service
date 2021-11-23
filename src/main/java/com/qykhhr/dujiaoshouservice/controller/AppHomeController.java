4f61be3c6e225968338d50a1d9dc054317b42049
qykhhr <201471966@qq.com>
2021-11-23 21:23:16 +0800

    first-commit hot-fix

--- src/main/java/com/qykhhr/dujiaoshouservice/controller/AppHomeController.java
+++ src/main/java/com/qykhhr/dujiaoshouservice/controller/AppHomeController.java
@@ -18,6 +18,7 @@ public class AppHomeController {
     @AccessLimit(seconds = 3, maxCount = 1) //15秒内 允许请求3次
     public R getImageList(){
         AppHomeData appHome = appHomeService.findAppHome();
+        System.out.println("hello git4");
         return R.ok().data("appHome",appHome);
     }
 
